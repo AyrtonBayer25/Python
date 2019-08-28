e5a74d67ce8756429319d682b88afb2e5a6f2212
AyrtonBayer <46352240+AyrtonBayer25@users.noreply.github.com>
2019-08-27 21:07:32 -0400

    Delete Class.9.py

--- Class.9.py
+++
@@ -1,7 +0,0 @@
-import os
-import csv
-os.chdir(os.path.dirname(__file__)
-
-abc = os.path.join("..","Resources","netflix_ratings.csv")
-with open(abc, newline ="") as abcd:
-    xyz=csv.reader(abcd,delimiter=',')

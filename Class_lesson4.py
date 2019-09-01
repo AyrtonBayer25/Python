375f0fa4bf9d0285e4977991759d0ec6de1bf799
AyrtonBayer <46352240+AyrtonBayer25@users.noreply.github.com>
2019-09-01 15:27:12 -0400

    Delete Class_lesson4.py

--- Class_lesson4.py
+++
@@ -1,7 +0,0 @@
-my_info = {
-    "name": "Ayrton Bayer",
-    "occupation": "student",
-    "hobbies": ["soccer", "volleyball", "football", "basketball"]
-}
-print(f'Hello my name is {my_info["name"]} and I am a {my_info["occupation"]}')
-print(f'I like to play {my_info["hobbies"]}')
